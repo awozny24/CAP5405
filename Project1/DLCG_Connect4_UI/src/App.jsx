44b37a52721a73961c5513ff337e9705933e2e6a
chintan-27 <chintan.acharya27@gmail.com>
2022-10-04 00:00:09 -0400

    Started with making AI in Web UI

--- Project1/DLCG_Connect4_UI/src/App.jsx
+++ Project1/DLCG_Connect4_UI/src/App.jsx
@@ -9,21 +9,95 @@ class App extends React.Component {
     super(props);
     this.state = { modal: [] };
   }
+
   predict = async (arr) => {
     // this.model = await tf.loadLayersModel("./tfjsmodel/model.json");
-    let prediction = this.model.predict(tf.tensor2d(arr, [1, 42]));
-    let predictionObject = prediction.arraySync()[0];
-    let res = 0;
-    for (let i in predictionObject) {
-      if (predictionObject[i] > predictionObject[res]) {
-        res = i;
+    // let prediction = this.model.predict(tf.tensor2d(arr, [1, 42]));
+    // let predictionObject = prediction.arraySync()[0];
+    // let res = 0;
+    // for (let i in predictionObject) {
+    //   if (predictionObject[i] > predictionObject[res]) {
+    //     res = i;
+    //   }
+    // }
+    // console.log(predictionObject);
+    // return res;
+    let transpose = [
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+    ];
+    let full = true;
+    for(let i = 0; i < 6; i++){
+      for(let j = 0; j < 7; j++){
+        if(transpose[i][j] == 0){
+          full = false;
+          break;
+        }
       }
     }
-    console.log(predictionObject);
-    return res;
+    if(full){
+      return 4;
+    }
+
+    for(let i = 0; i < 6; i++){
+      for(let j = 0; j < 7; j++){
+        // console.log(i,j)
+        transpose[i][j] = arr[j][i];
+      }
+    }
+    transpose = transpose.reverse();
+     // Checking Win Horizontanlly
+     for(let i in transpose){
+      for(let j = 0; j < 3; j++){
+        if(transpose[i][j] == 1 && transpose[i][j+1] == 1 && transpose[i][j+2] == 1 && transpose[i][j+3] == 1){
+          return 1;
+        }else if(transpose[i][j] == -1 && transpose[i][j+1] == -1 && transpose[i][j+2] == -1 && transpose[i][j+3] == -1){
+          return 2;
+        }
+      }
+     }
+
+     // Checking Win Vertically
+     for(let i in arr){
+      for(let j = 0; j < 2; j++){
+        if(arr[i][j] == 1 && arr[i][j+1] == 1 && arr[i][j+2] == 1 && arr[i][j+3] == 1){
+          return 1;
+        }else if(arr[i][j] == -1 && arr[i][j+1] == -1 && arr[i][j+2] == -1 && arr[i][j+3] == -1){
+          return 2;
+        }
+      }
+     }
+
+     // Checking Win Diagonally 1
+     for(let i = 0; i < 3; i++){
+      for(let j = 0; j < 4; j++){
+        if(transpose[i][j] == 1 && transpose[i+1][j+1] == 1 && transpose[i+2][j+2] == 1 && transpose[i+3][j+3] == 1){
+          return 1;
+        }else if(transpose[i][j] == -1 && transpose[i+1][j+1] == -1 && transpose[i+2][j+2] == -1 && transpose[i+3][j+3] == -1){
+          return 2;
+        }
+      }
+     }
+
+     // Checking Win Diagonally 2
+     for(let i = 0; i < 3; i++){
+      for(let j = 6; j > 2; j--){
+        if(transpose[i][j] == 1 && transpose[i+1][j-1] == 1 && transpose[i+2][j-2] == 1 && transpose[i+3][j-3] == 1){
+          return 1;
+        }else if(transpose[i][j] == -1 && transpose[i+1][j-1] == -1 && transpose[i+2][j-2] == -1 && transpose[i+3][j-3] == -1){
+          return 2;
+        }
+      }
+     }
+
+    return 0;
   };
   async componentDidMount() {
-    this.model = await tf.loadLayersModel("./tfjsmodel/model.json");
+    // this.model = await tf.loadLayersModel("./tfjsmodel/model.json");
   }
   alertModal = (title) => {
     this.setState({ modal: [<Modal key={title} title={title} />] });
